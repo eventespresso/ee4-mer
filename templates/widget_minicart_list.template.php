9f8f4df1e484abdb50f9ee6643d6d32f20b29c22
Brent Christensen <brent@eventespresso.com>
2015-03-20 15:02:51 -0700

    initial template refactor (incomplete) [touch:7438]

--- templates/widget_minicart_list.template.php
+++ templates/widget_minicart_list.template.php
@@ -1,12 +1,4 @@
 
-<?php echo $before_widget;?>
-
-	<?php echo $title; ?>
-	
-	<?php foreach ( $mini_cart as $cart_type => $cart ) { ?>
-			
-	<h4><?php echo $cart['title'];?></h4>
-	
 <div class="ui-widget-content  ui-corner-all">
 	<ul id="mini-cart-<?php echo $cart_type;?>-ul" class="mini-cart-ul">
 	<?php if ( $cart['has_items'] ) { ?>
@@ -29,23 +21,76 @@
 				?>
 			</li>
 		<?php endif; ?>
-	<?php } else { ?>	
+	<?php } else { ?>
 		<li><?php echo __( $cart['empty_msg'], 'event_espresso');?></li>
 	<?php } ?>
 	</ul>
 </div>
-	
-	<?php } ?>
-	
-	<h6 class="event-queue-grand-total right clearfix">
-		<span class="event-queue-grand-total-spn"><?php echo __('Grand Total: ', 'event_espresso'); ?></span>
-		<span class="event-queue-grand-total-items-spn"><?php printf(  _n( '%s attendee  ', '%s attendees ', $total_items, 'event_espresso' ), $total_items ); ?></span>
-		<span class="event-queue-grand-total-price-spn"><?php echo $currency_symbol . $grand_total; ?></span>
-	</h6>
-	
-	<p><a class="minicart-view-event-queue-btn show-if-js button ui-priority-primary ui-state-default ui-corner-all add-hover-fx" href="<?php echo $view_event_queue_url;?>">
-		<span class="ui-icon ui-icon-cart"></span>&nbsp;<?php echo $view_event_queue_text;?>
-	</a></p>
 
-<?php echo $after_widget;?>
+<?php
+/** @type string $before_widget */
+/** @type string $after_widget */
+/** @type string $before_title */
+/** @type string $after_title */
+/** @type string $title */
+/** @type string $register_url */
+/** @type string $event_queue */
+/** @type string $view_event_queue_url */
+/** @type string $events_list_url */
+/** @type int $total_items */
+
+echo $before_widget;
+echo $before_title . $title . $after_title;
+?>
+
+<div id="mini-cart-widget-dv" class="small-text">
+
+	<form id="mini-cart-qty-frm" action="<?php echo $register_url; ?>" method="POST">
+		<input type="hidden" name="event_queue" value="update">
+
+		<div id="mini-cart-wrap-dv" class="mini-cart-wrap-dv">
+
+			<ul id="mini-cart-ul" class="mini-cart-ul" style="width:100%;">
+
+				<tr id="mini-cart-tbl-row-hdr" class="mini-cart-tbl-row">
+				<ul class="mini-cart-line-item-ul">
+					<th class="mini-cart-tbl-price-th jst-rght" colspan="2"><?php echo __( 'Price', 'event_espresso' ); ?></th>
+					<th class="mini-cart-tbl-qty-th jst-rght"><?php echo __( 'Qty', 'event_espresso' ); ?></th>
+					<th class="mini-cart-tbl-total-th jst-rght"><?php echo __( 'Total', 'event_espresso' ); ?></th>
+				</tr>
+				</thead>
+
+				<tbody>
+				<?php echo $event_queue; ?>
+				</tbody>
+
+			</ul>
+		</div>
+
+		<div id="mini-cart-whats-next-buttons" class="mini-cart-whats-next-buttons">
+
+			<?php if ( $total_items ) { ?>
+
+				<span class="tiny-text">
+				<a class="mini-cart-view-cart-lnk view-cart-lnk mini-cart-button button" href="<?php echo $view_event_queue_url; ?>">
+					<span class="dashicons dashicons-cart"></span><?php echo __( 'view event queue', 'event_espresso' ); ?>
+				</a>
+			</span>
+				<br/>
+				<span class="tiny-text">
+				<a class="mini-cart-register-button mini-cart-button button" href="<?php echo $register_url; ?>">
+					<?php echo __( 'proceed to registration', 'event_espresso' ); ?>
+					<span class="dashicons dashicons-arrow-right-alt2"></span>
+				</a>
+			</span>
+
+			<?php } // if ( $total_items ) ?>
+
+		</div>
+
+	</form>
+
+</div>
+
+<?php echo $after_widget; ?>
 
