c0ab7daf0bdc11fd9cfcea77d78eb1a58c2c12d5
Brent Christensen <brent@eventespresso.com>
2015-03-31 20:23:23 -0700

    add filterable btn class to button css; [touch:7875]

--- templates/cart_results_modal_dialog.template.php
+++ templates/cart_results_modal_dialog.template.php
@@ -6,6 +6,7 @@
 /** @type string $events_list_url */
 /** @type string $register_url */
 /** @type string $current_cart */
+/** @type string $btn_class */
 ?>
 <a class="close-modal-js close-espresso-notice">&times;</a>
 <div id="cart-results-modal-dv">
@@ -13,14 +14,15 @@
 		<h3><?php echo $results; ?></h3>
 	</div>
 	<div id="cart-results-whats-next-buttons-dv">
-		<span class="tiny-text">
-			<a class="cart-results-go-back-button mini-cart-button button<?php echo $close_modal; ?>" href="<?php echo
+		<span class="cart-results-button-spn">
+			<a class="cart-results-go-back-button mini-cart-button button<?php echo $close_modal . ' ' . $btn_class; ?>"
+			   href="<?php echo
 			$events_list_url; ?>">
 				<span class="dashicons dashicons-arrow-left-alt2"></span><?php echo apply_filters( 'FHEE__EED_Multi_Event_Registration__return_to_events_list_btn_txt', __( 'Return to Events List', 'event_espresso' ) ); ?>
 			</a>
 		</span>
-		<span class="tiny-text">
-			<a class="cart-results-register-button mini-cart-button button" href="<?php echo $register_url; ?>">
+		<span class="cart-results-button-spn">
+			<a class="cart-results-register-button mini-cart-button button <?php echo $btn_class; ?>" href="<?php echo $register_url; ?>">
 				<?php echo apply_filters( 'FHEE__EED_Multi_Event_Registration__proceed_to_registration_btn_txt', __( 'Proceed to Registration', 'event_espresso' ) ); ?>
 				<span class="dashicons dashicons-arrow-right-alt2"></span>
 			</a>
@@ -29,7 +31,7 @@
 	<div id="cart-results-current-cart-dv">
 		<h5><?php echo $current_cart; ?></h5>
 		<div id="cart-results-view-cart-dv">
-			<a class="mini-cart-view-cart-lnk view-cart-lnk mini-cart-button button" href="<?php echo $view_event_cart_url; ?>">
+			<a class="mini-cart-view-cart-lnk view-cart-lnk mini-cart-button button <?php echo $btn_class; ?>" href="<?php echo $view_event_cart_url; ?>">
 				<span class="dashicons dashicons-cart"></span><?php echo apply_filters( 'FHEE__EED_Multi_Event_Registration__view_event_cart_btn_txt', sprintf( __( 'view %s', 'event_espresso' ), $event_cart_name ) ); ?>
 			</a>
 		</div>
